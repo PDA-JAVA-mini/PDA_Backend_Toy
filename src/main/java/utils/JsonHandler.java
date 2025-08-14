185fd8705eb3a637060a91561b950562d64d5ee0
PangHae <yds05074@gmail.com>
2025-08-14 15:38:54 +0900

    feat(json handler): json handler abstract class

---
+++ src/main/java/utils/JsonHandler.java
@@ -0,0 +1,32 @@
+package utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+
+public abstract class JsonHandler {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    JsonHandler() {
+        mapper.setPropertyNamingStrategy(new PropertyNamingStrategies.SnakeCaseStrategy());
+    }
+
+    public abstract <T> void write(T object, String filePath);
+
+    public abstract <T> T read(Class<T> clazz, String filePath);
+
+    public <T> String serialize(T object) throws RuntimeException{
+        try{
+            return mapper.writeValueAsString(object);
+        }catch(Exception e){
+            throw new RuntimeException("Failed to serialize", e);
+        }
+    }
+
+    public <T> T deserialize(String json, Class<T> clazz) throws RuntimeException {
+        try {
+            return mapper.readValue(json, clazz);
+        }catch(Exception e){
+            throw new RuntimeException("Failed to deserialize", e);
+        }
+    }
+}
