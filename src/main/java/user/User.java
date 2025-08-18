e8bb38030cb7214ce7077cb1feb2ccf468f31711
Mr-Tongyoung <154494090+Mr-Tongyoung@users.noreply.github.com>
2025-08-18 14:25:25 +0900

    hotfix:user

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -26,23 +26,10 @@ public class User {
     this.tripsIds = new ArrayList<>();
   }
 
-  public User(int id, String loginId, String password, String nickname, List<Integer> tripsIds) {
-    this.id = id;
-    this.loginId = loginId;
-    this.password = password;
-    this.nickname = nickname;
-    this.tripsIds = tripsIds;
-  }
 
   public int getId() { return id; }
   public String getLoginId() { return loginId; }
   public String getPassword() { return password; }
   public String getNickname() { return nickname; }
   public List<Integer> getTripIds() { return tripsIds; }
-
-  public void setId(int id) { this.id = id; }
-  public void setLoginId(String login_id) { this.loginId = login_id; }
-  public void setPassword(String password) { this.password = password; }
-  public void setNickname(String nickname) { this.nickname = nickname; }
-  public void setTripIds(List<Integer> trips_ids) { this.tripsIds = trips_ids; }
 }
